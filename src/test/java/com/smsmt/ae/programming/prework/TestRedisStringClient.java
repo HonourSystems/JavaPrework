9bed39c89dc5fc1e88dda02bdab9873f194c6b9e
Thomas Ackling <thomas.ackling@gmail.com>
2017-02-12 19:35:52 +1030

    update test to fail until implemented

--- src/test/java/com/smsmt/ae/programming/prework/TestRedisStringClient.java
+++ src/test/java/com/smsmt/ae/programming/prework/TestRedisStringClient.java
@@ -2,11 +2,14 @@ package com.smsmt.ae.programming.prework;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.fail;
+
 public class TestRedisStringClient {
 
     @Test
     public void TestPublishOnSet() {
         // TODO: Test that the RedisPublishingStringClient publishes the key value when calling set.
+        fail();
     }
 
 }
